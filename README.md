8b6134682049f760e395c389ffe5c0bff51a7e22
ondrakoupil <koupil@optimato.cz>
2015-06-21 12:17:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ČSOB platební brána
+
+Viz [https://github.com/csob/paymentgateway][1]
+
+[1]: https://github.com/csob/paymentgateway
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+define("TMP_TEST_DIR", __DIR__ . "/temp");
+
+\Tester\Environment::setup();
